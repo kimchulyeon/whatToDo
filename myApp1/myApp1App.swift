5ec5bd8d5e068a1cbb38e1e1da236d3c83d806b2
chulyeon <guinness987@gmail.com>
2023-05-04 16:31:06 +0900

    Initial Commit

---
+++ myApp1/myApp1App.swift
@@ -0,0 +1,20 @@
+//
+//  myApp1App.swift
+//  myApp1
+//
+//  Created by chulyeon kim on 2023/05/04.
+//
+
+import SwiftUI
+
+@main
+struct myApp1App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
